9614bb1d727b9d7eab1c16522d6f9b8eaccf914c
michael-mbugua <kamirimichael369@gmail.com>
2023-03-29 11:05:22 +0300

    set up and test the connection to the db

---
+++ Back-end/backend/index.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose, { connect } from 'mongoose'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import tourRoute from './Routes/tour.js'
+
+dotenv.config()
+const app = express()
+const port=process.env.PORT || 8000
+// for testing
+
+// app.get('/',(req,res)=>{
+//     res.send('API is working')
+// })
+
+// database connection setting
+
+const connection= async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
+        console.log("MongoDB well connected")
+    } catch (error) {
+        console.log('connection to mongoDB failed')
+    }
+}
+mongoose.set('strictQuery',false)
+
+
+
+connection();
+
+
+
+
+// middle ware
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use('/tours',tourRoute)
+
+app.listen(port,()=>{
+    console.log("Server Listening to port:", port)
+})
